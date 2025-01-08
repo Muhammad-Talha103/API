1a874cd2d87e20f31830bfd1656e47e30ee7600d
Muhammad Talha <Muhammad-Talha103@users.noreply.github.com>
2025-01-08 18:25:26 +0500

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
 
 import React from 'react'
 import {Poppins} from 'next/font/google'
+import Link from 'next/link'
 
 const poppins = Poppins({
   subsets: ['latin'],
   weight: ['400','500','600','700']
 })
+
 const Home = () => {
   return (
-    <div className='flex items-center justify-center '>
-        <button className={`${poppins.className} bg-green-800 text-white hover:text-black w-8 h-4 rounded-lg hover:bg-transparent border border-gray-600`}>GET API</button>
-    </div>
+    <Link href='/api/products' className='flex items-center justify-center h-screen'>
+        <button className={`${poppins.className} bg-green-800 font-semibold text-white hover:text-black p-3 rounded-lg hover:bg-transparent border border-gray-600`}>GET API</button>
+    </Link>
   )
 }
 
