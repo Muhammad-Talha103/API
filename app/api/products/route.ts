cb76b9280492eb259dce0bae29015959091fd1fb
Muhammad Talha <Muhammad-Talha103@users.noreply.github.com>
2025-01-08 19:26:40 +0500

    first commit

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -142,5 +142,103 @@ export const GET = () => {
       price: 290,
       oldPrice: 325,
     },
+    {
+      id: 11,
+      tag: "Trending Products",
+      title: "Marquis Lounge Chair",
+      description: "The Marquis Lounge Chair offers unmatched comfort and sophisticated design, making it perfect for any modern living room or office space.",
+      category: "Chair",
+      rating: 4.8,
+      stock: 100,
+      image: "blob:https://www.imagine.art/13e7acfd-1f28-4ab2-ba17-6c31d8438b27",
+      price: 600,
+      oldPrice: 810
+    },
+    {
+      id: 12,
+      tag: "Trending Products",
+      title: "Aspen Recliner",
+      description: "The Aspen Recliner is designed for ultimate relaxation, featuring smooth reclining capabilities and a comfortable cushion to help you unwind.",
+      category: "Chair",
+      rating: 4.1,
+      stock: 100,
+      image: "https://i.pinimg.com/originals/be/6d/cc/be6dcc2acfa4c585d59609b60e8907d4.png",
+      price: 500,
+      oldPrice: 645
+    },
+    {
+      id: 13,
+      tag: "Trending Products",
+      title: "Polaris chair",
+      description: "The Polaris Chair offers an ergonomic design with excellent back support, making it perfect for long hours of sitting, whether at work or home.",
+      category: "Chair",
+      rating: 4.1,
+      stock: 100,
+      image: "https://toppng.com/uploads/preview/modern-chair-11530974705y7wprbkgxz.png",
+      price: 500,
+      oldPrice: 645
+    },
+    {
+      id: 14,
+      tag: "Trending Products",
+      title: "Noble Executive Chair",
+      description: "The Noble Executive Chair combines luxury with comfort, offering premium materials and a stylish design suitable for office or home use.",
+      category: "Chair",
+      rating: 4.1,
+      stock: 100,
+      image: "https://w7.pngwing.com/pngs/611/345/png-transparent-chair-chair-png-transparent-image-isolated-image-transparent-background-remove-the-background-product-image-free-image-clipping-path-clipping-paths-thumbnail.png",
+      price: 390,
+      oldPrice: 500
+    },
+    {
+      id: 15,
+      tag: "Top Categories",
+      title: "Modern LCW Chair",
+      description: "The Modern LCW Chair features sleek design with a comfortable seating experience, perfect for contemporary workspaces and stylish home offices.",
+      category: "Chair",
+      rating: 3.7,
+      stock: 100,
+      image: "https://icon2.cleanpng.com/20240330/rgc/transparent-metal-frame-comfortable-green-fabric-office-chair-with-wheels6608cf202cbe02.30819262.webp",
+      price: 570,
+      oldPrice: 660
+    },
+    {
+      id: 16,
+      tag: "Top Categories",
+      title: "Vintage LCW Chair",
+      description: "The Vintage LCW Chair blends classic design with comfort, offering a retro look that's ideal for any space needing an elegant touch.",
+      category: "Chair",
+      rating: 4.3,
+      stock: 100,
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkyPbX9Uaf1RPVscuVHDwD6jlmUtF6a0OhLw&s',
+      price: 630,
+      oldPrice: 700
+    },
+    {
+      id: 17,
+      tag: "Top Categories",
+      title: "Eco LCW Chair",
+      description: "The Eco LCW Chair offers sustainable design without compromising on comfort, perfect for eco-conscious individuals seeking a modern look.",
+      category: "Chair",
+      rating: 4.9,
+      stock: 100,
+      image: 'https://images.rawpixel.com/image_png_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvcC00NjctcG9tLTAzMjUucG5n.png',
+      price: 700,
+      oldPrice: 820
+    },
+    {
+      id: 18,
+      tag: "Top Categories",
+      title: "Premium LCW Chair",
+      description: "The Premium LCW Chair combines luxurious materials with excellent ergonomics, providing unmatched comfort and style for both work and relaxation.",
+      category: "Chair",
+      rating: 4.8,
+      stock: 100,
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6qpFXehFlI0vJYkppLgxv6Uaiy7jyrdjh_A&s',
+      price: 620,
+      oldPrice: 760
+    }
+    
+    
   ]);
 };
